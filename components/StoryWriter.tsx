bebd3bbda6ded1d05abc188d97dae8fcd0daa0ad
Sahil5505 <sahilrevankar20co21@gmail.com>
2024-08-29 20:53:00 +0530

    gpt api calls--

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -2,21 +2,124 @@
 
 
 import React from 'react'
+import { useState } from 'react'
 import { Textarea } from './ui/textarea'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 import { Button } from './ui/button'
+import { Frame } from '@gptscript-ai/gptscript'
 
 const StoryWriter = () => {
+  
+  const [story, setStory] = useState<string>("");
+  const [pages, setPages] = useState<number>();  
+ const [progress, setProgress] = useState("");    
+ const [runStarted, setRunStarted] = useState<boolean>(false);   
+ const [runFinished, setRunFinished] = useState<boolean | null>(null); 
+ const [currentTool, setCurrentTool] = useState(""); 
+ const [events, setEvents] = useState<Frame[]>([]); 
+ 
+ 
+
+
+ const storiesPath = "/stories";
+
+
+   async function runScript() {
+
+    setRunStarted(true);
+    setRunFinished(false);
+
+
+    const response = await fetch("/api/run-script", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+       },
+       body:JSON.stringify({story, pages , path: storiesPath}),
+    });
+
+    if (response.ok && response.body) {
+      //Handle streams from the API
+      //..
+      console.log("Streaming had started");
+
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder();  
+
+      handleStream(reader, decoder);
+
+    } else {
+      setRunFinished(true);
+      setRunStarted(false);
+      console.log("An error occured while generating the story. Please try again later  "); 
+    }
+
+
+   }
+
+   async function handleStream(reader: ReadableStreamDefaultReader<Uint8Array>, decoder: TextDecoder) {
+    // manage the stream from api
+    while(true) {
+      const {done, value} = await reader.read();
+
+      if(done) break; // breaks out of the infinite loop
+
+      // the decoder is used to decode the uint8array into a string
+
+      const chunk = decoder.decode(value, {stream: true}) ;
+
+      //we split the chunks into evens by splitting it by the event: keyword.
+      const eventData = chunk.split("\n\n").filter((line) => line.startsWith("event: ")).map((line) => line.replace(/^event: /, ""));
+      
+      //we parse the json data and update the state accordingly.
+      eventData.forEach(data => {
+        try {
+          const parsedData = JSON.parse(data);
+          //console.log(parsedData);  
+          if (parsedData.type === "callProgress") {
+            setProgress(
+              parsedData.output[parsedData.output.length -1].content
+            );
+            setCurrentTool(parsedData.tool?.description || "");
+          }else if (parsedData.type === "callStart") {
+            setCurrentTool(parsedData.tool?.description || ""); 
+          }else if (parsedData.type === "runFinish") {
+            setRunFinished(true); 
+            setRunStarted(false);
+          } else {
+            setEvents((prevEvents) => [...prevEvents, parsedData]);
+          }
+
+        } catch (error) {
+          console.error("failed to parse JSON", error);
+
+        }
+      })
+
+    }
+
+
+   }
+
+
+
   return (
     <div className='flex flex-col container'>
         
 
         <section  className='flex-1 flex flex-col border border-purple-300 rounded-md p-10 space-y-2'>
-          <Textarea className='flex-1 text-black'
+          <Textarea 
+
+          value={story}
+          onChange={(e) => setStory(e.target.value)}  
+          
+          
+           
+          className='flex-1 text-black'
           placeholder='Write a story about a robot and a human who become friends......'
           />
-          <Select>
-            <SelectTrigger>
+          <Select onValueChange={value => setPages(parseInt(value))}>
+            <SelectTrigger> 
           <SelectValue placeholder="How many pages should the story be?"/>
             </SelectTrigger>
 
@@ -29,21 +132,55 @@ const StoryWriter = () => {
               ))}
 
               
-            </SelectContent>
+            </SelectContent> 
 
 
           </Select>
 
-          <Button className='w-full bg-black' size="lg">
+          <Button disabled={!story || !pages || runStarted} className='w-full bg-black' size="lg"
+          onClick={runScript}
+          >
             Generate Story
           </Button>
 
         </section>
 
         <section className='flex-1 pb-5 mt-5'>
+          <div className='flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font font-mono p-10 h-96 overflow-y-auto'>
+          
+            <div>
+              {runFinished === null && (
+                <>
+                <p className='animate-pulse mr-5'>Im waiting for you to Generate a story above...</p>
+                <br />
+                </>
+              )}
+              <span className='mr-5'>{">>"}</span>
+                {progress}
+            </div>
+
+            {/*current tool*/}
+            {currentTool && (
+              <div className='py-10'>
+                <span>{"--- [Current Tool] ---"}</span> 
+                {currentTool}
+              </div>
+            )}
+            {/*Render Events*/}
 
 
-        </section>
+            {runStarted && (
+              <div>
+                <span className='mr-5 animate-in'>
+                    {"--- [AI StoryTeller Has Started] ---"}
+                </span>
+                <br />
+
+              </div>
+            )}
+
+          </div>
+        </section> 
 
 
         </div>
